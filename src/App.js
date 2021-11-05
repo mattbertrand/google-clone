f65e8122cb8607e923e13312c8aea7e7fa9515ae
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-11-05 12:13:16 -0700

    added first routes using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-			<h1>Google Clone</h1>
-			<Home />
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/search" element={<h1>This is the search page</h1>}>	
+				</Route>
+			</Routes>
+		</Router>
     </div>
   );
 }
