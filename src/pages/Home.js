42cd4e1dcaae0877339e0bdae3d35439f3c722e8
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-11-05 11:57:10 -0700

    installed material ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,19 @@ function Home() {
 	return (
 		<div className="home">
 			<h1>This is the home page yo</h1>
+			<div className="home__header">
+				<div className="home__headerLeft">
+				
+				</div>
+				
+				<div className="home__headerRight">
+				
+				</div>
+			</div>
+
+			<div className="home__body">
+
+			</div>
 		</div>
 	)
 }
